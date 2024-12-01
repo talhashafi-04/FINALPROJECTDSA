65e91720c3bbf48eb69e8c2704ef4d7327fcda02
Talha-Shafi <i230563@isb.nu.edu.pk>
2024-12-01 15:57:14 +0500

    AVL proto done

---
+++ Source.cpp
@@ -0,0 +1,28 @@
+#include "AVL.h"
+
+
+
+int main()
+{
+
+	
+	String rootFile = ""; 
+
+
+	rootFile = AVL::insert(rootFile, "data1", "50")->key ;
+	rootFile = AVL::insert(rootFile, "data2", "30")->key ;
+	rootFile = AVL::insert(rootFile, "data3", "70")->key ;
+	rootFile = AVL::insert(rootFile, "data4", "40")->key ;
+	rootFile = AVL::insert(rootFile, "data5", "35")->key ;
+	rootFile = AVL::insert(rootFile, "data6", "20")->key ;
+
+           	 
+ 
+      
+	int a;
+
+
+
+
+	return 0; 
+}
